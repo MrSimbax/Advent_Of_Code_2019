e8a26630d155cd634be000803e20ba796b0f2162
MrSimbax <matt@przybyl.io>
2019-12-09 21:58:57 +0100

    Improve performance

--- day9.jl
+++ day9.jl
@@ -1,11 +1,11 @@
 using OffsetArrays
 
 mutable struct Program
-    raw_memory::Array{BigInt,1} # memory
-    memory::OffsetArray{BigInt,1} # memory indexed from 0
+    raw_memory::Array{Int,1} # memory
+    memory::OffsetArray{Int,1} # memory indexed from 0
     
-    in::Array{BigInt,1} # input queue
-    out::Array{BigInt,1} # output queue
+    in::Array{Int,1} # input queue
+    out::Array{Int,1} # output queue
 
     ip::Int # instruction pointer
     rb::Int # relative base
@@ -26,61 +26,63 @@ Program(srccode, in, out) = Program(srccode,
     [])
 
 # Memory access functions
-function expand_memory(program::Program, pos)
+function expand_memory!(program::Program, pos)
     if pos >= length(program.memory)
-        append!(program.raw_memory, zeros(Int, pos - length(program.raw_memory) + 1))
+        # append!(program.raw_memory, zeros(Int, pos - length(program.raw_memory) + 1))
+        resize!(program.raw_memory, pos + 1)
+        @views fill!(program.raw_memory[(length(program.memory) + 1):end], 0)
         program.memory = OffsetVector(program.raw_memory, 0:(length(program.raw_memory) - 1))
     end
 end
 
-function memget(program::Program, pos)
-    expand_memory(program, pos)
+function memget!(program::Program, pos)
+    expand_memory!(program, pos)
     return program.memory[pos]
 end
 
-function memset(program::Program, pos, value)
-    expand_memory(program, pos)
+function memset!(program::Program, pos, value)
+    expand_memory!(program, pos)
     program.memory[pos] = value
 end
 
 # Reading/writing parameters
-function load(program::Program)
-    param = memget(program, program.ip + program.offset)
+function load!(program::Program)
+    param = memget!(program, program.ip + program.offset)
     pmode = get(program.pmodes, program.offset, 0)
     program.offset += 1
 
     if pmode == 0
         # position mode
-        return memget(program, param)
+        return memget!(program, param)
     elseif pmode == 1
         # immediate mode
         return param
     elseif pmode == 2
         # relative mode
-        return memget(program, param + program.rb)
+        return memget!(program, param + program.rb)
     else
         error("Bad parameter mode for load: $pmode")
     end
 end
 
-function store(program::Program, value)
-    param = memget(program, program.ip + program.offset)
+function store!(program::Program, value)
+    param = memget!(program, program.ip + program.offset)
     pmode = get(program.pmodes, program.offset, 0)
     program.offset += 1
 
     if pmode == 0
         # position mode
-        memset(program, param, value)
+        memset!(program, param, value)
     elseif pmode == 2
         # relative mode
-        memset(program, param + program.rb, value)
+        memset!(program, param + program.rb, value)
     else
         error("Bad parameter mode for store: $pmode")
     end
 end
 
 function decode_opcode(opcode)
-    pmodes = []
+    pmodes = Array{Int,1}()
     if opcode > 99
         pmodes = Int.(digits(div(opcode, 100)))
         opcode = mod(opcode, 100)
@@ -90,60 +92,60 @@ end
 
 function run_program!(program::Program)::Program
     while program.ip < length(program.memory)
-        opcode, program.pmodes = decode_opcode(memget(program, program.ip))
+        opcode, program.pmodes = decode_opcode(memget!(program, program.ip))
         program.offset = 1
         if opcode == 1
             # add
-            store(program, load(program) + load(program))
+            store!(program, load!(program) + load!(program))
             program.ip += 4
         elseif opcode == 2
             # mult
-            store(program, load(program) * load(program))
+            store!(program, load!(program) * load!(program))
             program.ip += 4
         elseif opcode == 3
             # input
             if isempty(program.in)
                 return program
             end
-            store(program, pop!(program.in))
+            store!(program, pop!(program.in))
             program.ip += 2
         elseif opcode == 4
             # output
-            pushfirst!(program.out, load(program))
+            pushfirst!(program.out, load!(program))
             program.ip += 2
         elseif opcode == 5
             # jump-if-true
-            if load(program) != 0
-                program.ip = load(program)
+            if load!(program) != 0
+                program.ip = load!(program)
             else
                 program.ip += 3
             end
         elseif opcode == 6
             # jump-if-false
-            if load(program) == 0
-                program.ip = load(program)
+            if load!(program) == 0
+                program.ip = load!(program)
             else
                 program.ip += 3
             end
         elseif opcode == 7
             # less than
-            if load(program) < load(program)
-                store(program, 1)
+            if load!(program) < load!(program)
+                store!(program, 1)
             else
-                store(program, 0)
+                store!(program, 0)
             end
             program.ip += 4
         elseif opcode == 8
             # equals
-            if load(program) == load(program)
-                store(program, 1)
+            if load!(program) == load!(program)
+                store!(program, 1)
             else
-                store(program, 0)
+                store!(program, 0)
             end
             program.ip += 4
         elseif opcode == 9
             # adjust rb
-            program.rb += load(program)
+            program.rb += load!(program)
             program.ip += 2
         elseif opcode == 99
             # halt
@@ -162,17 +164,17 @@ end
 
 function main()
     open("input9.txt", "r") do inputfile
-        srccode = [parse(BigInt, x) for x in split(strip(read(inputfile, String)), ",")]
+        srccode = [parse(Int, x) for x in split(strip(read(inputfile, String)), ",")]
 
-        out = Array{BigInt,1}()
+        out = Array{Int,1}()
         program = Program(copy(srccode), [1], out)
         run_program!(program)
         println("PART 1:")
         print_output(out)
 
-        out = Array{BigInt,1}()
+        out = Array{Int,1}()
         program = Program(copy(srccode), [2], out)
-        run_program!(program)
+        @time run_program!(program)
         println("PART 2:")
         print_output(out)
     end
